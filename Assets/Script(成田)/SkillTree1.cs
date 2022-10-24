d01613abc3a21a4382a42187a38d964f0e2c3f91
naritayuuto <yuuto122narita@i.softbank.jp>
2022-10-24 12:18:55 +0900

    Skilltreeをきれいにしたもの

---
+++ Assets/Script(成田)/SkillTree1.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum SkillId
+{
+    heal = 1,
+    attack,
+    buff
+}
+public class SkillTree1 : MonoBehaviour
+{
+    SkillManager skillManager = null;//skillpointを使うため。
+
+    Skilltree parent;//一つ上。
+
+    List<SkillTree1> childs = new List<SkillTree1>();//自分自身。このSkilltreeが持っているスキル。
+
+    [SerializeField, SerializeReference, SubclassSelector]
+    ISkill skill;//最初から持っておく。
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        skillManager = GameObject.FindGameObjectWithTag("SkillManager").GetComponent<SkillManager>();
+    }
+
+    public void SkillPointJudge(float skillpoint,int arraynumber)//ポイントが足りているか、skillの配列順番
+    {
+        if (skillManager.SkillPoint < skillpoint)
+        {
+            return;
+        }
+        else
+        {
+            skillManager.Buttons[arraynumber].Yobidasi(skill);
+        }
+    }
+
+    public virtual void SkillAction() { }
+}
