ac55e0d8db3754d7a608deec7a5feb6875a9cda7
naritayuuto <yuuto122narita@i.softbank.jp>
2022-11-07 12:36:30 +0900

    攻撃したときにSkillManagerを参照してスキルポイントを保存するようにした SkillManagerの無駄な変数を削除。

--- Assets/Script(成田)/Attackjudge.cs
+++ Assets/Script(成田)/Attackjudge.cs
@@ -9,13 +9,15 @@ public class Attackjudge : MonoBehaviour
     PlayerController player = null;
     TextMeshProUGUI damageText = null;
     Skilltree skilltree = null;
-    EnemyController1 enemy;
+    EnemyController enemy;
+    SkillManager sManager = null; 
     private void Start()
     {
         if(!damageUi)
         {
             Debug.LogError("damageUIがありません");
         }
+        sManager = GameObject.FindGameObjectWithTag("SkillManager").GetComponent<SkillManager>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         damageText = damageUi.GetComponentInChildren<TextMeshProUGUI>();
         //skilltree = GameObject.FindGameObjectWithTag("Skilltree").GetComponent<Skilltree>();
@@ -26,11 +28,11 @@ public class Attackjudge : MonoBehaviour
         {
             Debug.Log("Attack");
             Vector3 hitPos = other.ClosestPointOnBounds(transform.position);
+            damageText.text = player.AttackDamage.ToString();
+            Instantiate(damageUi, hitPos, Quaternion.identity);//ダメージ表示
             //enemy = other.gameObject.GetComponent<EnemyController1>();
             //enemy.Damage(player.AttackDamage);//ダメージを与える
-            //skilltree.Skillpoint += 0.5f;//スキルpoint加算
-            damageText.text = player.AttackDamage.ToString();
-            Instantiate(damageUi,hitPos,Quaternion.identity);//ダメージ表示
+            sManager.SkillPoint += 0.5f;//スキルpoint加算    
         }
     }
 }
--- Assets/Script(成田)/SkillManager.cs
+++ Assets/Script(成田)/SkillManager.cs
@@ -2,24 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //ここではskillbuttonにそれぞれ何のスキルを持つのか割り振りする。
-//skilltreeとこれは別物。
 public class SkillManager : MonoBehaviour//skillを管理する。目次のようなもので何が解放されているのかの情報を持っている。
 {
     [SerializeField]
-    SkillButton[] buttons;//SkillButtonが持つskillnumberを記録するために使用する。_skillActiveの要素番号に結びつけ。
+    SkillButton[] buttons;//SkillButtonが持つarraynumberを記録するために使用する。_skillActiveの要素番号に結びつけ。
 
     bool[] _skillActive;//直接的にいじることはない
 
-    List<SkillTree1> skillTree = new List<SkillTree1>();
+    List<SkillTree> skillTree = new List<SkillTree>();
 
     float skillPoint = 0f;
 
-    ///<summary>healスキルの数</summary>
-    private int healcount = 1;
-    ///<summary>attackスキルの数</summary>
-    private int attackcount = 2;
-    ///<summary>buffスキルの数</summary>
-    private int buffcount = 3;
     public float SkillPoint { get => skillPoint; set => skillPoint = value; }
     public bool[] SkillActive { get => _skillActive; set => _skillActive = value; }
     public SkillButton[] Buttons { get => buttons;}
@@ -29,23 +22,8 @@ public class SkillManager : MonoBehaviour//skillを管理する。目次のようなもので何
         _skillActive = new bool[buttons.Length];
         for (int i = 0; i < buttons.Length; i++)
         {
-            if (i < healcount)//healのスキル
-            {
-                buttons[i].Skillnumber = i + 1;
-                buttons[i].SkillId = SkillId.heal;
-            }
-            else if (i < attackcount + healcount)//attackのスキル
-            {
-                buttons[i].Skillnumber = i - healcount + 1;
-                buttons[i].SkillId = SkillId.attack;
-            }
-            else//buffのスキル
-            {
-                buttons[i].Skillnumber = i - attackcount;
-                buttons[i].SkillId = SkillId.buff;
-            }
             buttons[i].ArrayNumber = i;
-            buttons[i].Skillpoint += 2 * buttons[i].Skillnumber;
+            buttons[i].Skillpoint += 2 * buttons[i].ArrayNumber;
         }
     }
 }
