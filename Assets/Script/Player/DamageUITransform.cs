137415dba934dcc52ef78390907e893d5047b922
naritayuuto <yuuto122narita@i.softbank.jp>
2023-02-11 15:49:49 +0900

    プレイヤーが攻撃したときに攻撃が当たった場所を取得し、ダメージを与えた時にその場所に表示するようにするscript。 攻撃用の武器の当たり判定とは別にプレイヤー前方に当たり判定を出現させれば動きます。

---
+++ Assets/Script/Player/DamageUITransform.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageUITransform : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _attackJudgeObject = null;
+    Attackjudge _attackJudge = null;
+    private void Start()
+    {
+        _attackJudge = _attackJudgeObject.GetComponent<Attackjudge>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            Vector3 hitPos = other.ClosestPointOnBounds(transform.position);
+            if(_attackJudge)
+            {
+                _attackJudgeObject.SetActive(true);
+                //_attackJudge._uiTransform = hitPos;
+            }
+        }
+    }
+}
