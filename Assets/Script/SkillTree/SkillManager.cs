1a19ae361e2e14f41b9b096a05ef8b10dd524bc2
naritayuuto <yuuto122narita@i.softbank.jp>
2023-02-12 14:56:16 +0900

    スキルポイントを画面内のテキストに表示

--- Assets/Script/SkillTree/SkillManager.cs
+++ Assets/Script/SkillTree/SkillManager.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 //ここではskillbuttonにそれぞれ何のスキルを持つのか割り振りする。
 public class SkillManager : MonoBehaviour//skillを管理する。目次のようなもので何が解放されているのかの情報を持っている。
 {
-    [SerializeField, Tooltip("SkillButtonが付いているCanvas")]
+    [SerializeField,Header("SkillButtonが付いているCanvas"),Tooltip("SkillButtonが付いているCanvas")]
     GameObject _skillCanvas = null;
+    [SerializeField, Header("スキルポイントを表示するText")]
+    TextMeshProUGUI _skillPointText = null;
     [Tooltip("SkillTreeが付いたボタン")]
     List<SkillButton> _skillButtons = new List<SkillButton>();//SkillButtonが持つarraynumberを記録するために使用する。_skillActiveの要素番号に結びつけ。
     [Tooltip("どのスキルが解放されたかを保持する物")]
@@ -21,18 +25,16 @@ public class SkillManager : MonoBehaviour//skillを管理する。目次のようなもので何
     public float SkillPoint { get => _skillPoint; set => _skillPoint = value; }
     public bool[] SkillActive { get => _skillActive; set => _skillActive = value; }
 
-    private void Awake()
-    {
-        GameManager.Instance.GetSkillManager(this);
-    }
     void Start()
     {
         _skillCanvas.SetActive(true);
+        _skillPointText.text = "スキルポイント : " + _skillPoint.ToString();
         GameObject[] obj = GameObject.FindGameObjectsWithTag("Skilltree");
         foreach(var buttons in obj)
         {
             _skillButtons.Add(buttons.GetComponent<SkillButton>());
             _skillTree.Add(buttons.GetComponent<SkillTree>());
+            buttons.GetComponent<SkillTree>().SkillManager = this;
         }
         _skillActive = new bool[_skillButtons.Count];
         for (int i = 0; i < _skillButtons.Count; i++)
@@ -42,8 +44,13 @@ public class SkillManager : MonoBehaviour//skillを管理する。目次のようなもので何
         //最初からfalseだと要素番号が振られないため
         _skillCanvas.SetActive(false);
     }
+    /// <summary>
+    /// enemyを攻撃した場合に呼ぶ関数
+    /// </summary>
+    /// <param name="enemyDie"></param>
     public void AddSkillPoint(bool enemyDie)
     {
-        _skillPoint = enemyDie == true ? _skillPoint + _attackSkillPoint : _skillPoint + _defeatSkillPoint;
+        _skillPoint = enemyDie == true ? _skillPoint + _defeatSkillPoint : _skillPoint + _attackSkillPoint;
+        _skillPointText.text = "スキルポイント : " + _skillPoint.ToString("F0");
     }
 }
