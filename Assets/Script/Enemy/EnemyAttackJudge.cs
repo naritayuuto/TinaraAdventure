bd9326460008553967f5cc481988e9193a83fbf7
naritayuuto <yuuto122narita@i.softbank.jp>
2023-02-15 16:33:36 +0900

    当たり判定内にいるobjectから取れるか試すcomponentを変更。 自身の当たり判定をtrueにする関数と与えられたTransformに移動する関数を作り、武器が当たった時に呼ぶようにしました。

--- Assets/Script/Enemy/EnemyAttackJudge.cs
+++ Assets/Script/Enemy/EnemyAttackJudge.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class EnemyAttackJudge : MonoBehaviour
 {
+    [SerializeField, Header("攻撃力"), Tooltip("Enemyの攻撃力")]
     int _enemyAttackDamage = 500;
+    [Tooltip("定期的に当たり判定を消す処理を行うためのタイマー")]
+    float _timer = 0;
+    [Tooltip("当たり判定内に標的が入っていない場合に当たり判定を消す秒数")]
+    float _colliderActiveTime = 0.5f;
+    Collider _collider = default;
+    Vector3 _iTransform;
+    private void Start()
+    {
+        _collider = GetComponent<Collider>();
+        _collider.enabled = false;
+    }
+    private void Update()
+    {
+        if (_collider.enabled == true)
+        {
+            _timer += Time.deltaTime;
+            if (_timer >= _colliderActiveTime)
+            {
+                _collider.enabled = false;
+                _timer = 0;
+            }
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.TryGetComponent<PlayerController>(out PlayerController player))
+        if (other.TryGetComponent<PlayerHp>(out PlayerHp playerHp))
         {
-            player._playerHp.Damage(_enemyAttackDamage);
-            player._playerAnimAndcollider.DamageAnimation();
+            playerHp.Damage(_enemyAttackDamage);
         }
+        transform.position = _iTransform;
+        _collider.enabled = false;
+    }
+
+    public void TransformMove(Vector3 pos)
+    {
+        _iTransform = transform.position;
+        transform.position = pos;
+    }
+
+    public void ColliderActive()
+    {
+        _collider.enabled = true; 
     }
 }
