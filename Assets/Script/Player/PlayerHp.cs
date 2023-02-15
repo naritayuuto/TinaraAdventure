d4357534f7bd755700e65e15a1a71f7e15041361
naritayuuto <yuuto122narita@i.softbank.jp>
2023-02-15 16:41:31 +0900

    デバッグ用の処理を削除。 ダメージアニメーションを流すように変更。

--- Assets/Script/Player/PlayerHp.cs
+++ Assets/Script/Player/PlayerHp.cs
@@ -8,7 +8,7 @@ public class PlayerHp : MonoBehaviour
     //PlayerController player = null;
     [SerializeField, Header("体力の最大値"), Tooltip("playerの体力の最大値")]
     private float _playerHp = 5000f;
-    [Header("ダメージを受けて変化する体力"), Tooltip("playerの増減する体力")]
+    [SerializeField, Header("ダメージを受けて変化する体力"), Tooltip("playerの増減する体力")]
     float _playerDamageHp = 0;
     [SerializeField, Tooltip("プレイヤーのHP表示用テキスト")]
     Text playerHpText = null;
@@ -25,15 +25,13 @@ public class PlayerHp : MonoBehaviour
     void Update()
     {
         playerHpText.text = _playerDamageHp.ToString() + "/" + _playerHp.ToString();
-        if (Input.GetButtonDown("Jump"))
-        {
-            Damage(500);
-        }
     }
     public void Damage(int damage)
     {
         _playerDamageHp = _playerDamageHp > damage ? _playerDamageHp - damage : 0;
 
         hpSlider.value = _playerDamageHp / _playerHp;
+        GameManager.Instance.Player._playerAnimAndcollider.DamageAnimation();
+        Debug.Log(_playerDamageHp);
     }
 }
