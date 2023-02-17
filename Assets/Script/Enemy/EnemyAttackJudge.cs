f559362fde2048b0b26a58b3252c02e710cdb2fc
naritayuuto <yuuto122narita@i.softbank.jp>
2023-02-17 16:11:24 +0900

    攻撃判定の仕様変更により、必要なくなった関数、変数の削除。

--- Assets/Script/Enemy/EnemyAttackJudge.cs
+++ Assets/Script/Enemy/EnemyAttackJudge.cs
@@ -6,47 +6,18 @@ public class EnemyAttackJudge : MonoBehaviour
 {
     [SerializeField, Header("攻撃力"), Tooltip("Enemyの攻撃力")]
     int _enemyAttackDamage = 500;
-    [Tooltip("定期的に当たり判定を消す処理を行うためのタイマー")]
-    float _timer = 0;
-    [Tooltip("当たり判定内に標的が入っていない場合に当たり判定を消す秒数")]
-    float _colliderActiveTime = 0.5f;
     Collider _collider = default;
-    Vector3 _iTransform;
     private void Start()
     {
         _collider = GetComponent<Collider>();
         _collider.enabled = false;
-    }
-    private void Update()
-    {
-        if (_collider.enabled == true)
-        {
-            _timer += Time.deltaTime;
-            if (_timer >= _colliderActiveTime)
-            {
-                _collider.enabled = false;
-                _timer = 0;
-            }
-        }
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<PlayerHp>(out PlayerHp playerHp))
         {
             playerHp.Damage(_enemyAttackDamage);
+            _collider.enabled = false;
         }
-        transform.position = _iTransform;
-        _collider.enabled = false;
-    }
-
-    public void TransformMove(Vector3 pos)
-    {
-        _iTransform = transform.position;
-        transform.position = pos;
-    }
-
-    public void ColliderActive()
-    {
-        _collider.enabled = true; 
     }
 }
--- Assets/Script/Player/Attackjudge.cs
+++ Assets/Script/Player/Attackjudge.cs
@@ -10,13 +10,8 @@ public class Attackjudge : MonoBehaviour//武器に付ける
     TextMeshProUGUI damageText = null;
     [Tooltip("playerのAttackDamageの値を入れる変数")]
     int _playerAttack = 0;
-    [Tooltip("定期的に当たり判定を消す処理を行うためのタイマー")]
-    float _timer = 0;
-    [Tooltip("当たり判定内に標的が入っていない場合に当たり判定を消す秒数")]
-    float _colliderActiveTime = 0.5f;
     [Tooltip("当たり判定")]
     Collider _collider = default;
-    public Vector3 _iTransform;
     private void Start()
     {
         if (!damageUi)
@@ -27,39 +22,17 @@ public class Attackjudge : MonoBehaviour//武器に付ける
         _collider = GetComponent<Collider>();
         _collider.enabled = false;
     }
-    private void Update()
-    {
-        if (_collider.enabled == true)
-        {
-            _timer += Time.deltaTime;
-            if (_timer >= _colliderActiveTime)
-            {
-                _collider.enabled = false;
-                _timer = 0;
-            }
-        }
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<EnemyHp>(out EnemyHp enemyHp))
         {
+            Vector3 hitPos = other.ClosestPointOnBounds(transform.position);
             _playerAttack = (int)Mathf.Round(GameManager.Instance.Player._playerAttackParam.AttackDamage);
             damageText.text = _playerAttack.ToString();
-            Instantiate(damageUi, transform.position, Quaternion.identity);//ダメージ表示
+            Instantiate(damageUi, hitPos, Quaternion.identity);//ダメージ表示
             enemyHp.Damage(_playerAttack);
             GameManager.Instance.SkillManager.AddSkillPoint(enemyHp.Die);
-            transform.position = _iTransform;
             _collider.enabled = false;
         }
-    }
-    public void TransformMove(Vector3 pos)
-    {
-        _iTransform = transform.position;
-        transform.position = pos;
-    }
-
-    public void ColliderActive()
-    {
-        _collider.enabled = true;
     }
 }
