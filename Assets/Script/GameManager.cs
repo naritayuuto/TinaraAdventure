1a96cca5ab1f7b416716145c0ececb2b8a15d160
naritayuuto <yuuto122narita@i.softbank.jp>
2023-03-22 10:41:07 +0900

    プレイヤーに付けたcomponentの参照が多かったのでgamemanagerから参照できるように変更。

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,18 +5,28 @@ using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager _instance = default;
+    static GameManager _instance = default;
 
     public static GameManager Instance { get => _instance;}
 
     [SerializeField, Header("スキルマネージャー")]
     SkillManager _skillManager = null;
 
-    [SerializeField,Header("PlayerPrefab"),Tooltip("プレイヤーのオブジェクト")]
+    [Tooltip("プレイヤー")]
     GameObject _player = null;
 
+    PlayerAnim _playerAnim = null;
+    PlayerAttackParam _playerAttackParam = null;
+    PlayerHp _playerHp = null;
+    PlayerController _playerController = null;
+    PlayerUseSkill _playerUseSkill = null;
     public SkillManager SkillManager { get => _skillManager; }
     public GameObject Player { get => _player; }
+    public PlayerAnim PlayerAnim { get => _playerAnim; }
+    public PlayerAttackParam PlayerAttackParam { get => _playerAttackParam; }
+    public PlayerHp PlayerHp { get => _playerHp; }
+    public PlayerController PlayerController { get => _playerController; }
+    public PlayerUseSkill PlayerUseSkill { get => _playerUseSkill; }
 
     private void Awake()
     {
@@ -30,5 +40,10 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
         _player = GameObject.FindGameObjectWithTag("Player");
+        _playerController = _player.GetComponent<PlayerController>();
+        _playerAnim = _player.GetComponent<PlayerAnim>();
+        _playerAttackParam = _player.GetComponent<PlayerAttackParam>();
+        _playerHp = _player.GetComponent<PlayerHp>();
+        _playerUseSkill = _player.GetComponent<PlayerUseSkill>();
     }
 }
