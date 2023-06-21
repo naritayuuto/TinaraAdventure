c8619b2f6e4038c1c246eff80ee607a6d356eb73
naritayuuto <yuuto122narita@i.softbank.jp>
2023-06-22 08:31:03 +0900

    死んだときの処理や、ダメージを受けたときの処理を変更した。

--- Assets/Script/Enemy/Skeleton/EnemyHp.cs
+++ Assets/Script/Enemy/Skeleton/EnemyHp.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.AI;
 public class EnemyHp : MonoBehaviour
 {
     [SerializeField,Header("体力の最大値"),Tooltip("体力の最大値")]
@@ -13,24 +13,34 @@ public class EnemyHp : MonoBehaviour
     bool _die = false;
     [SerializeField]
     Slider hpSlider = null;
+    Animator _anim = null;
     public bool Die { get => _die; }
     public float Hp { get => _enemyDamageHp; set => _enemyDamageHp = value; }
     // Start is called before the first frame update
     private void Start()
     {
         _enemyDamageHp = _enemyHp;
+        _anim = GetComponent<Animator>();
     }
     // Update is called once per frame
     public void Damage(int damage)
-    {
+    {     
         _enemyDamageHp = _enemyDamageHp <= damage ? 0 : _enemyDamageHp - damage;
         hpSlider.value = _enemyDamageHp / _enemyHp;
         if (_enemyDamageHp <= 0)
         {
             _die = true;
-            //アニメーションを流す、当たり判定となっているコライダーのリストを作っておき、当たり判定を消す。
+            _anim.Play("Die");
+            GetComponent<IEnemy>().Die();
             Debug.Log("敵が死んだ");
-            Destroy(gameObject);
         }
+        else
+        {
+            _anim.Play("Damage");
+        }
+    }
+    public void EnemyDie()
+    {
+        Destroy(gameObject);
     }
 }
