ab080fc04829ee499424e86636b73a2d92c9dc09
naritayuuto <yuuto122narita@i.softbank.jp>
2023-06-22 08:30:12 +0900

    プレイヤーが生成範囲から出た場合、一定時間経つと生成された敵が消えるようにした。

--- Assets/Script/Enemy/EnemyGenerator.cs
+++ Assets/Script/Enemy/EnemyGenerator.cs
@@ -16,6 +16,12 @@ public class EnemyGenerator : MonoBehaviour
     float _generatPosX = 0f;
     [SerializeField, Header("Enemyを生成する横範囲")]
     float _generatPosZ = 0f;
+    [SerializeField, Tooltip("プレイヤーが範囲外に出てからEnemyが生きていられる秒数")]
+    float _exitTime = 30f;
+    float _timer = 0f;
+    
+    bool _exitCheck = false;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -26,35 +32,51 @@ public class EnemyGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _timer = _exitCheck ? _timer += Time.deltaTime : 0;
 
+        if(_timer >= _exitTime && _exitCheck)
+        {
+            foreach (var enemy in _enemys)
+            {
+                Destroy(enemy);
+            }
+            _timer = 0;
+            _exitCheck = false;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            Debug.Log("入った");
             Generat();
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            foreach(var enemy in _enemys)
-            {
-                Destroy(enemy);
-            }
-        }
+        if(other.CompareTag("Player"))
+        _exitCheck = true;
+        Debug.Log("出た");
     }
     void Generat()
     {
-        for (int i = 0; i < _generatedNum; i++)
+        if (!_exitCheck)
         {
-            _generatPosX = Random.Range(_pos.x - _generatPosX, _pos.x + _generatPosX);
-            _generatPosZ = Random.Range(_pos.z - _generatPosZ, _pos.z + _generatPosZ);
-            GameObject enemy = Instantiate(_enemyPrefab, new Vector3(_generatPosX, _pos.y, _generatPosZ), Quaternion.identity);
-            _enemys[i] = enemy;
+            for (int i = 0; i < _generatedNum; i++)
+            {
+                float generatPosX = Random.Range(_pos.x - _generatPosX, _pos.x + _generatPosX);
+                float generatPosZ = Random.Range(_pos.z - _generatPosZ, _pos.z + _generatPosZ);
+                GameObject enemy = Instantiate(_enemyPrefab, new Vector3(generatPosX, _pos.y, generatPosZ), Quaternion.identity);
+                _enemys[i] = enemy;
+            }
+            Debug.Log("生成");
+        }
+        else
+        {
+            Debug.Log("生成失敗");
+            _exitCheck = false;
         }
     }
 }
