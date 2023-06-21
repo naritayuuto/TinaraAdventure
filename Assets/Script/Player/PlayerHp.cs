65b4fbd34e1cdb4e397cbf57e07991527285d859
naritayuuto <yuuto122narita@i.softbank.jp>
2023-06-22 08:34:49 +0900

    変数の名前の変更、及びHPがゼロになった時にGameManager側の関数を呼ぶように変更した。

--- Assets/Script/Player/PlayerHp.cs
+++ Assets/Script/Player/PlayerHp.cs
@@ -7,7 +7,7 @@ public class PlayerHp : MonoBehaviour
     //[SerializeField]
     //PlayerController player = null;
     [SerializeField, Header("体力の最大値"), Tooltip("playerの体力の最大値")]
-    private float _playerHp = 5000f;
+    private float _playerMaxHp = 5000f;
     [SerializeField, Header("ダメージを受けて変化する体力"), Tooltip("playerの増減する体力")]
     float _playerDamageHp = 0;
     [Tooltip("攻撃無効スキルを使っている時に受けた攻撃をカウントする")]
@@ -21,16 +21,18 @@ public class PlayerHp : MonoBehaviour
     /// <summary> playerの増減する体力</summary>
     public float PlayerDamageHp { get => _playerDamageHp; set => _playerDamageHp = value; }
     /// <summary> playerの体力の最大値</summary>
-    public float PlayerMaxHp { get => _playerHp; set => _playerHp = value; }
+    public float PlayerMaxHp { get => _playerMaxHp; set => _playerMaxHp = value; }
+    public Slider HpSlider { get => hpSlider; set => hpSlider = value; }
+
     void Start()
     {
-        _playerDamageHp = _playerHp;
+        _playerDamageHp = _playerMaxHp;
         _playerUseSkill = GameManager.Instance.PlayerUseSkill;
         _playerAnim = GameManager.Instance.PlayerAnim;
     }
     void Update()
     {
-        playerHpText.text = _playerDamageHp.ToString() + "/" + _playerHp.ToString();
+        playerHpText.text = _playerDamageHp.ToString() + "/" + _playerMaxHp.ToString();
     }
     public void Damage(int damage)
     {
@@ -46,9 +48,21 @@ public class PlayerHp : MonoBehaviour
         else
         {
             _playerDamageHp = _playerDamageHp > damage ? _playerDamageHp - damage : 0;
-            hpSlider.value = _playerDamageHp / _playerHp;
-            _playerAnim.DamageAnimation();
+            HpSlider.value = _playerDamageHp / _playerMaxHp;
+            if (_playerDamageHp > 0)
+            {
+                _playerAnim.DamageAnimation();
+            }
+            else
+            {
+                _playerAnim.Die();
+                Die();
+            }
             Debug.Log(_playerDamageHp);
         }
+    }
+    public void Die()
+    {
+        GameManager.Instance.FadeOUT();
     }
 }
